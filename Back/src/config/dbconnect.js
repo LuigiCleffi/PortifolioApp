9fc51a5d7bce9546965f9023ef43cda3d373b5d0
LuigiCff <65309377+LuigiCleffi@users.noreply.github.com>
2023-01-30 17:18:55 -0300

    Front

--- Back/src/config/dbconnect.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-mongoose.set('strictQuery', true);
-mongoose.connect("mongodb+srv://luigi:9hL0FR4eZwD1WcRn@portifolio.6ntesa5.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true })
-
-let db = mongoose.connection
-
-export default db
-
