86044b10dd959fd24c0b0d5e87fe5c89388ddae3
Luigi Cleffi <65309377+LuigiCleffi@users.noreply.github.com>
2023-01-30 17:01:55 -0300

    Delete src directory

--- src/@types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import 'styled-components'
-import { defaultTheme } from '../Assets/Styles/themes/default'
-
-type ThemeType = typeof defaultTheme
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
-}
--- src/Assets/Styles/themes/default.ts
+++
@@ -1,7 +0,0 @@
-export const defaultTheme = {
-    "gradient-components": "linear-gradient(270deg, rgb(19, 173, 199) 0%, rgb(148, 93, 214) 100%)",
-    "gradient-components-hover": "linear-gradient(270deg, #4cc3d8 0%, rgb(180, 131, 239) 100%)", 
-    "gradient-Titles": "linear-gradient(to right, #ffffff, #c5c5c5 , #868686, #686868, #434343)",
-    "gradient-Title-subsection": "linear-gradient(to right, #ffffff, #EFF5F5,#D6E4E5,#F7F7F7)",
-    white: "#ffff"
-}
